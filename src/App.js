71fef0590930e2362aea3574099eb77001e896de
Susana Mayo Cano <susana.mayo@betterconsultants.es>
2020-01-23 09:25:09 +0100

    Add dynamic filtering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import SearchDB from './Components/SearchDB';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
--- src/Components/SearchDB.js
+++ src/Components/SearchDB.js
@@ -1,23 +1,97 @@
-import React from 'react';
-import  {retrieveDB} from '../Database';
+import React, { useState, useEffect } from 'react';
+import * as d3 from "d3";
+import sound_recording from '../files/sound_recordings.csv';
+import sound_recording_input_report from '../files/sound_recordings_input_report.csv';
+import {
+    Spinner,
+    Form,
+    Table
+} from 'react-bootstrap';
+
 function SearchDB() {
 
-    const song = { "artist": "Ed Sheeran", "name": "Shape of you", "isrc": "765543", "duration": "655" };
-    var songInput = [];
-    songInput = retrieveDB();
-    console.log(songInput);
-    for(var i = 0; i < songInput.length; i ++){
-        console.log("Song " + songInput[i]);
-    }
-    return (
-        <div className="div_container">
-            <p>Load searchDB</p>
-            <select>
+    const [soundRecording, setSoundRecording] = useState([]);
+    const [soundRecordingInputReport, setSoundRecordingInputRecord] = useState([]);
+    const [filteredResults, setFilteredResults] = useState([]);
 
-                {/* {songInput.map(song => {
-                    return (<option key={song.isrc}value={song.isrc}>{song.title} - {song.artist} </option>);
-                })} */}
-            </select>
+    useEffect(() => {
+        d3.csv(sound_recording, function (res) {
+            console.log("soundRecording ", res)
+            setSoundRecording(res);
+        });
+
+        d3.csv(sound_recording_input_report, function (res) {
+            console.log("soundRecordingInputReport: ", res)
+            setSoundRecordingInputRecord(res);
+            setFilteredResults(res);
+        });
+    }, []);
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+
+        var result = soundRecordingInputReport;
+        if (value !== "-1") {
+            const objectToFilter = soundRecording[value];
+            result = soundRecordingInputReport.filter(function (song) {
+                return song.title.includes(objectToFilter.title);
+            });
+        }
+        setFilteredResults(result);
+    }
+
+    const renderOptions = () => {
+        var result = [];
+        for (var i = 0; i < soundRecording.length; i++) {
+            const song = soundRecording[i];
+            result.push(<option value={i}>{song.title} - {song.artist}</option>);
+        }
+        return result;
+    }
+
+
+
+    return (
+        <div className="container_div">
+            {soundRecording === undefined || soundRecording.length === 0 ?
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+                :
+                <>
+
+                    <Form.Group controlId="soundRecordingSelect">
+                        <Form.Label>Please select one song</Form.Label>
+                        <Form.Control as="select" onChange={handleChange}>
+                            <option value="-1">Choose one...</option>
+                            {renderOptions()}
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Artist</th>
+                                <th>ISRC</th>
+                                <th>Duration</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredResults.map(song => {
+                                return (
+                                    <tr>
+                                        <td>{song.title}</td>
+                                        <td>{song.artist}</td>
+                                        <td>{song.isrc}</td>
+                                        <td>{song.duration}</td>
+                                    </tr>)
+                            })}
+                        </tbody>
+                    </Table>
+                </>
+            }
+
         </div>
     );
 }
