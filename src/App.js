0fb372d2e7eab753e4bb9e425ce995ae1d8dbdb6
asdy <adoptdontbuy2019@gmail.com>
2020-01-27 21:44:50 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
