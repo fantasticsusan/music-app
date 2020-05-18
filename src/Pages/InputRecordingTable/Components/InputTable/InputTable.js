1a047d792bca50a65d2f8b2b4346047a554ed398
asdy <adoptdontbuy2019@gmail.com>
2020-05-18 12:23:43 +0200

    Fix margin

--- src/Pages/InputRecordingTable/Components/InputTable/InputTable.js
+++ src/Pages/InputRecordingTable/Components/InputTable/InputTable.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import {
-    Row,
-    Col
-} from 'react-bootstrap'
 import {EMPTY_RECORDING} from '../../../../Utils/const'
 import {Spinner, Table, Badge} from '../../../../Components'
 
@@ -38,29 +34,29 @@ class InputTable extends React.Component {
 
         return (
             <React.Fragment>
-                <Row>
-                    <Col>
+                <div className="columns">
+                    <div className="column">
                         <div className="table-header table-header-input">
                             <h2 className="subtitle"><i className="fas fa-file-alt"/> INPUT</h2>
                         </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column">
                         <div className="items-result">
                             <p><Badge>{inputRecordings.length}</Badge> recordings left to be matched</p>
                         </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={12}>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="column is-full">
                         <Table
                             elements={inputRecordings}
                             selectedRecording={selectedRecording}
                             handleClick={this.handleClick}
                         />
-                    </Col>
-                </Row>
+                    </div>
+                </div>
             </React.Fragment>)
     }
 }
